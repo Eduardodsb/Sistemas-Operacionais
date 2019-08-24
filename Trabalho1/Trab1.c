3ced427d6cc90a9a8f75e3f3a92d7fd95dc63712
Eduardodsb <eduardo_b_bosa@hotmail.com>
2019-08-24 15:03:10 -0300

    +Escalonamento_FCFS

--- Trabalho1/Trab1.c
+++ Trabalho1/Trab1.c
@@ -10,19 +10,37 @@ compilação: gcc -o Trab1 Trab1.c -ansi -Wall
 
 #include<stdlib.h>
 #include<stdio.h>
+#include <stdbool.h>
 
+/*Estruturas*/
 typedef struct processo{ /*Estrutura que define um processo*/
-int id, temp_ingresso, temp_exec, prioridade;
+int id, prioridade;
+float temp_ingresso, temp_exec, temp_inicio_exec;
+bool executado;
 } processo;
+
+
+typedef struct escalonamento{ /*Estrutura que define informações sobre um tipo de escalonamento*/
+float Tt, Tw, trocas_Contexto, tempo_Total;
+} escalonamento;
 
 
 /*Potótipos*/
 processo* leituraDeDados(); 
 void imprimeProcessos(processo *processos, int N_processos);
+void escalonamento_FCFS(processo *processos, int N_processos, escalonamento *escalonador);
+
+/*Variáveis Globais*/
+int total_Processos;
+
 
 int main(int argc, char **argv){
     processo *processos;
+    escalonamento escalonadores[7];
+
     processos = leituraDeDados();
+
+    escalonamento_FCFS(processos, total_Processos, &escalonadores[0]);
     return 0;
 }
 
@@ -46,11 +64,12 @@ processo* leituraDeDados(){ /*Leitura da entrada e inicialização da estrutura 
 
     for(i = 0; i < N_processos; i++){
         processos[i].id = i;
-        fscanf (file, "%d", &processos[i].temp_ingresso);
+        processos[i].executado = false;
+        fscanf (file, "%f", &processos[i].temp_ingresso);
     }
 
     for(i = 0; i < N_processos; i++){
-        fscanf (file, "%d", &processos[i].temp_exec);
+        fscanf (file, "%f", &processos[i].temp_exec);
     }
 
     for(i = 0; i < N_processos; i++){
@@ -59,21 +78,28 @@ processo* leituraDeDados(){ /*Leitura da entrada e inicialização da estrutura 
 
     imprimeProcessos(processos, N_processos);    
     fclose(file);
+
+    total_Processos = N_processos;
     return processos;
 }
 
 void imprimeProcessos(processo *processos, int N_processos){ /*Imprime os processos criados*/
     int i;
     for(i = 0; i < N_processos; i++){
-        printf("%d ", processos[i].temp_ingresso);
+        printf("%d ", (int) processos[i].temp_ingresso);
     }
     printf("\n");
     for(i = 0; i < N_processos; i++){
-        printf("%d ", processos[i].temp_exec);
+        printf("%d ", (int) processos[i].temp_exec);
     }
     printf("\n");
     for(i = 0; i < N_processos; i++){
         printf("%d ", processos[i].prioridade);
     }
     printf("\n");
+}
+
+void escalonamento_FCFS(processo *processos, int N_processos, escalonamento *escalonador){
+    float linha_do_tempo = 0;
+
 }
