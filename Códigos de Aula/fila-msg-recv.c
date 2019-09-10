0e9d4f64094a9126105244ab5ada271708ceed90
Eduardodsb <eduardo_b_bosa@hotmail.com>
2019-09-09 22:35:09 -0300

    +Códigos de Aula

---
+++ Códigos de Aula/fila-msg-recv.c
@@ -0,0 +1,38 @@
+// Arquivo mq-recv.c: recebe mensagens de uma fila de mensagens POSIX.
+// Em Linux, compile usando: cc -o mq-recv -lrt mq-recv.c
+#include<stdio.h>
+#include<stdlib.h>
+#include<mqueue.h>
+#include<sys/stat.h>
+
+#define QUEUE "/my_queue"
+
+int main(void) {
+   mqd_t queue; // descritor da fila de mensagens
+   struct mq_attr attr; // atributos da fila de mensagens
+   int msg; // as mensagens são números inteiros
+
+   // abre ou cria a fila com permissoes 0666
+   if((queue = mq_open (QUEUE, O_RDWR|O_CREAT, 0666, &attr)) < 0){
+      perror ("mq_open");
+      exit (1);
+   }
+
+   // le o atributos default da fila de mensagens criada 
+   if (mq_getattr(queue, &attr) == -1) {
+      perror("mq_getattr");
+      exit (1);
+   }
+   printf("Maximum # of messages on queue:   %ld\n", attr.mq_maxmsg);
+   printf("Maximum message size:             %ld\n", attr.mq_msgsize);
+
+   // recebe cada mensagem e imprime seu conteudo
+   for (;;) {
+      //if ((mq_receive (queue, (void*) &msg, sizeof(msg), 0)) < 0)
+      if ((mq_receive (queue, (void*) &msg, attr.mq_msgsize, 0)) < 0)
+         perror("mq_receive:") ;
+      else
+         printf ("Received msg value %d\n", msg);
+   }
+   return 0;
+}
---
+++ Códigos de Aula/fila-msg-send.c
@@ -0,0 +1,32 @@
+// Arquivo mq-send.c: envia mensagens para uma fila de mensagens POSIX.
+// Em Linux, compile usando: cc -o mq-send -lrt mq-send.c
+#include<stdio.h>
+#include<stdlib.h>
+#include<mqueue.h>
+#include<unistd.h>
+
+#define QUEUE "/my_queue"
+
+int main() {
+   mqd_t queue; // descritor da fila
+   int  msg; // mensagem a enviar
+
+   // abre a fila de mensagens, se existir
+   if((queue = mq_open (QUEUE, O_RDWR)) < 0){
+      perror ("mq_open");
+      exit (1);
+   }
+
+   for (;;) {
+      //msg = random() % 100 ; // valor entre 0 e 99
+      msg = 2 ; // valor entre 0 e 99
+      // envia a mensagem
+      if (mq_send (queue, (void*) &msg, sizeof(msg), 0) < 0){
+         perror ("mq_send");
+         exit (1);
+      }
+      printf ("Sent message with value %d\n", msg);
+      sleep (1) ;
+   }
+   return 0;
+}
